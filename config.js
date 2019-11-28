b9d8f4c176fe0d88884ff720b4df134bcc479caa
Mateusz <cieslakowski.mat@gmail.com>
2019-11-28 16:58:42 +0100

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '263679647902-kj2a89fq41cd6h5der4a4b0qrqa4n7ci.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '6tBJUqjgp1Ie1FnRFDMka6bP',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
